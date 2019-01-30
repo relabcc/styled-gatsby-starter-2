eb0519f9a62d134e2988035bab3d5cfdcc4f716a
Renddi Yeh <renddi@relab.cc>
2019-01-30 16:02:53 +0800

    ssr ok

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,5 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react';
 import wrapWithProvider from './with-provider';
+import LanguageProvider from './src/i18n/LanguageProvider';
+
 export const wrapRootElement = wrapWithProvider;
+export const wrapPageElement = ({ element, props }) => <LanguageProvider {...props}>{element}</LanguageProvider>
