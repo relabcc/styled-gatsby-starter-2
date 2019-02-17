3c6fe4b641c6a33e04498f3aa962fa851e3209ee
Renddi Yeh <renddi@relab.cc>
2019-02-17 14:38:22 +0800

    updated redux-responsive setting

--- with-provider.js
+++ with-provider.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Provider } from 'react-redux';
+import { calculateResponsiveState } from 'redux-responsive'
 
 import ThemeProvider from './src/components/ThemeProvider';
 import creatStore from './src/stores/createStore';
 
+const store = creatStore();
+
+class InitialDispatch extends Component {
+  componentDidMount() {
+    if (typeof window !== 'undefined') {
+      store.dispatch(calculateResponsiveState(window))
+    }
+  }
+
+  render() {
+    return this.props.children
+  }
+}
+
 export default ({ element }) => (
-  <Provider store={creatStore()}>
-    <ThemeProvider>{element}</ThemeProvider>
+  <Provider store={store}>
+    <InitialDispatch>
+      <ThemeProvider>{element}</ThemeProvider>
+    </InitialDispatch>
   </Provider>
 )
